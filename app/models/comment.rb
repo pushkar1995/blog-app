8a51b5094e9500fa89613f16af60f17d2f7234d7
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-03 23:00:12 +0545

    Processing data in models code added in models according to the requirement of the project

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  # Callbacks
+  after_save :update_post_comments_counter
+
+  # Methods
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  # Callbacks
+  after_save :update_user_posts_counter
+
+  # Methods
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
 end
