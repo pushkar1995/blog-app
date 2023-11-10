b22b22e9ae75b63c6f448f30503020243d1a0d45
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-10 17:32:43 +0545

    Added required code to add views for the models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
-  def index; end
+  layout 'standard'
 
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
+
+    if @posts.any?
+      @posts = @posts.paginate(page: params[:page], per_page: 2)
+    else
+      flash[:notice] = 'No posts found for this user.'
+      redirect_to user_path(@user)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
