e56ae8e4b7cbf1b8303d678476a8a9a2749507b2
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-11-11 09:58:58 +0545

    Fixed the See all Posts button as mentioned by the reviewer without using paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,7 @@
 class PostsController < ApplicationController
-  layout 'standard'
-
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-
-    if @posts.any?
-      @posts = @posts.paginate(page: params[:page], per_page: 2)
-    else
-      flash[:notice] = 'No posts found for this user.'
-      redirect_to user_path(@user)
-    end
+    @posts = Post.where(author_id: params[:user_id])
   end
 
   def show
