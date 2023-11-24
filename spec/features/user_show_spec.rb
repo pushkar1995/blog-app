2bbb6b290a2c30f1d43d5a6a379f87b94d4ee910
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-24 16:04:17 +0100

    update Displays user Profile picture, username, bio and number of posts

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -1,40 +1,48 @@
-# require 'rails_helper'
+ require 'rails_helper'
 
-# RSpec.feature 'User show page' do
-#   let!(:user1) { User.create(name: 'Pushkar', bio: 'A student from Morocco', photo: 'https://avatars.githubusercontent.com/u/130588108?v=4') }
-#   let!(:post1) { Post.create(author: user1, title: 'Post 01') }
-#   let!(:post2) { Post.create(author: user1, title: 'Post 02') }
-#   let!(:post3) { Post.create(author: user1, title: 'Post 03') }
-#   let!(:post4) { Post.create(author: user1, title: 'Post 04') }
+ RSpec.feature 'User show page' do
 
-#   scenario 'Displays user Profile picture, username, bio and number of posts' do
-#     visit user_path(user1)
-#     expect(page).to have_css("img[src='#{user1.photo}']")
-#     expect(page).to have_content(user1.name)
-#     expect(page).to have_content(user1.bio)
-#     expect(page).to have_content("Number of posts: #{user1.posts_counter}")
-#   end
+  before each: do
+    @user=User.create(name: 'Pushkar', bio: 'A student from Morocco', photo: 'https://avatars.githubusercontent.com/u/130588108?v=4')
+    @post1=Post.create(author: user1, title: 'Post 01')
+    @post2=Post.create(author: user1, title: 'Post 02')
+    @post3=Post.create(author: user1, title: 'Post 03')
+    @post4=Post.create(author: user1, title: 'Post 04')
+    visit user_path(@user.id)
+    end
+  #  let!(:user1) {  }
+  #  let!(:post1) {  }
+  #  let!(:post2) { Post.create(author: user1, title: 'Post 02') }
+  #  let!(:post3) { Post.create(author: user1, title: 'Post 03') }
+  #  let!(:post4) { Post.create(author: user1, title: 'Post 04') }
 
-#   scenario 'Displays user 3 posts' do
-#     visit user_path(user1)
+   scenario 'Displays user Profile picture, username, bio and number of posts' do
+     expect(page).to have_css(".user-photo")
+     expect(page).to have_content(@user.name)
+     expect(page).to have_content(@user.bio)
+     expect(page).to have_content("Number of posts: #{user.posts_counter}")
+   end
 
-#     expect(page).to have_content(post1.title)
-#     expect(page).to have_content(post3.title)
-#   end
+  #  scenario 'Displays user 3 posts' do
+  #    visit user_path(user1)
 
-#   scenario 'Displays Button that lets me view all of a user posts, and redirects to the posts when clicked' do
-#     visit user_path(user1)
-#     expect(page).to have_link('See all posts')
-#     click_link 'See all posts'
-#     sleep(1)
-#     expect(current_path).to eq(user_posts_path(user1))
-#   end
+  #    expect(page).to have_content(post1.title)
+  #    expect(page).to have_content(post3.title)
+  #  end
 
-#   scenario 'click on user post redirects to that post show page.' do
-#     visit user_path(user1)
+  #  scenario 'Displays Button that lets me view all of a user posts, and redirects to the posts when clicked' do
+  #    visit user_path(user1)
+  #    expect(page).to have_link('See all posts')
+  #    click_link 'See all posts'
+  #    sleep(1)
+  #    expect(current_path).to eq(user_posts_path(user1))
+  #  end
 
-#     click_link post1.title
-#     sleep(1)
-#     expect(current_path).to eq(user_post_path(user1, post1))
-#   end
-# end
+  #  scenario 'click on user post redirects to that post show page.' do
+  #    visit user_path(user1)
+
+  #    click_link post1.title
+  #    sleep(1)
+  #    expect(current_path).to eq(user_post_path(user1, post1))
+  #  end
+ end
