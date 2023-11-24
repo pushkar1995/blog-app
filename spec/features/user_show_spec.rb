3a3aaaa42684c90a823f3cb5f1ebd03ffa10eb8b
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-24 16:28:47 +0100

    update the user spec

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -4,10 +4,10 @@
 
   before :each do
     @user=User.create(name: 'Pushkar', bio: 'A student from Morocco', photo: 'https://avatars.githubusercontent.com/u/130588108?v=4')
-    @post1=Post.create(author: user1, title: 'Post 01')
-    @post2=Post.create(author: user1, title: 'Post 02')
-    @post3=Post.create(author: user1, title: 'Post 03')
-    @post4=Post.create(author: user1, title: 'Post 04')
+    @post1=Post.create(author: @user, title: 'Post 01',comments_counter: 0, likes_counter: 0)
+    @post2=Post.create(author: @user, title: 'Post 02',comments_counter: 0, likes_counter: 0)
+    @post3=Post.create(author: @user, title: 'Post 03',comments_counter: 0, likes_counter: 0)
+    @post4=Post.create(author: @user, title: 'Post 04',comments_counter: 0, likes_counter: 0)
     visit user_path(@user.id)
     end
   #  let!(:user1) {  }
