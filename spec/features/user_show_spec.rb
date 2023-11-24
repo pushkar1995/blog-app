49eafd99fecec35917bf351174366b2a92648fc3
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-11-24 16:52:50 +0100

    update others user test

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -4,10 +4,10 @@
 
     before :each do
         @user=User.create(name: 'Pushkar', bio: 'A student from Morocco', photo: 'https://avatars.githubusercontent.com/u/130588108?v=4')
-        @post1=Post.create(author: @user, title: 'Post 01',comments_counter: 0, likes_counter: 0)
-        @post2=Post.create(author: @user, title: 'Post 02',comments_counter: 0, likes_counter: 0)
-        @post3=Post.create(author: @user, title: 'Post 03',comments_counter: 0, likes_counter: 0)
-        @post4=Post.create(author: @user, title: 'Post 04',comments_counter: 0, likes_counter: 0)
+        @post1=Post.create(author: @user, title: 'Hello Microverse',comments_counter: 0, likes_counter: 0)
+        @post2=Post.create(author: @user, title: 'Hi Java',comments_counter: 0, likes_counter: 0)
+        @post3=Post.create(author: @user, title: 'Ruby on Rails',comments_counter: 0, likes_counter: 0)
+        @post4=Post.create(author: @user, title: 'Gra_Madar',comments_counter: 0, likes_counter: 0)
         visit user_path(@user.id)
     end
 
@@ -18,20 +18,19 @@
     # expect(page).to have_content("Number of posts: #{@user.posts_counter}")
    end
 
-  #  scenario 'Displays user 3 posts' do
-  #    visit user_path(user1)
+    scenario 'Displays user 3 posts' do
+      expect(page).to have_content(post1.title)
+      expect(page).to have_content(post2.title)
+      expect(page).to have_content(post3.title)
+    end
 
-  #    expect(page).to have_content(post1.title)
-  #    expect(page).to have_content(post3.title)
-  #  end
-
-  #  scenario 'Displays Button that lets me view all of a user posts, and redirects to the posts when clicked' do
-  #    visit user_path(user1)
-  #    expect(page).to have_link('See all posts')
-  #    click_link 'See all posts'
-  #    sleep(1)
-  #    expect(current_path).to eq(user_posts_path(user1))
-  #  end
+    scenario 'Displays Button that lets me view all of a user posts, and redirects to the posts when clicked' do
+      visit user_path(user1)
+      expect(page).to have_link('See all posts')
+      click_link 'See all posts'
+      sleep(1)
+      expect(current_path).to eq(user_posts_path(@user.id))
+    end
 
   #  scenario 'click on user post redirects to that post show page.' do
   #    visit user_path(user1)
